7ef11e989d04cdccee255f08179f2704309bebca
Lucas Emilio <93223042+LucasEmilio@users.noreply.github.com>
2022-01-24 00:20:11 -0600

    Create nombre.py

---
+++ EjerciciosBasicos/Nombre/nombre.py
@@ -0,0 +1,10 @@
+#Programa el cual recibe un nombre, y regresa el nombre mas un saludo
+
+#Entrada de los datos
+name = input("Ingresa tu nombre:\n")
+
+#Proceso
+saludo = "Hola " + name
+
+#Salida de los datos
+print(f"{saludo}")
