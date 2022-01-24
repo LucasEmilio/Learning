790c2e82bbb0c85345b4fa90c318017dad52e210
Lucas Emilio <93223042+LucasEmilio@users.noreply.github.com>
2022-01-24 00:18:56 -0600

    Create conversion.py

---
+++ EjerciciosBasicos/Conversion/conversion.py
@@ -0,0 +1,10 @@
+#Programa el cual recibe un dato en centimetros y lo convierte en pulgadas
+ 
+#Entrada de los datos
+cm = int(input("Dame un valor en centimetros\n"))
+
+#Proceso
+inch = cm * 0.3937
+
+#Salida de los datos
+print(f"{cm} centimetros son: {inch} pulgadas")
