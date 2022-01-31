0dd73a444a100342dd34236ed8dabfc25ca7354b
Lucas Emilio <93223042+LucasEmilio@users.noreply.github.com>
2022-01-31 12:05:02 -0600

    Create dia.py

---
+++ condicionales/dia/dia.py
@@ -0,0 +1,19 @@
+#programa que nos dice que dia de la semana es
+op = int(input("Introduzca dia de la semana: "))
+
+if op ==1:
+	print("Lunes")
+elif op ==2:
+	print("Martes")
+elif op ==3:
+	print("Miercoles")
+elif op ==4:
+	print("Jueves")
+elif op ==5:
+	print("Viernes")
+elif op ==6:
+	print("Sabado")
+elif op ==7:
+	print("Domingo")
+else:
+	print("ERROR: Dia incorrecto.")
