5b8bfecf4ff117da6d6c96d4053cc0e4449a03c7
Lucas Emilio <93223042+LucasEmilio@users.noreply.github.com>
2022-01-31 12:02:21 -0600

    Create numNegPos.py

---
+++ condicionales/numNegPos/numNegPos.py
@@ -0,0 +1,10 @@
+#Datos de entrada
+num = int(input("Ingresa un numero entero: "))
+
+#proceso
+if num > 0:
+	#Datos de salida
+	print(f"[{num}] es un numero entero")
+else:
+	#Datos de salida
+	print(f"[{num}] es un numero negativo")
