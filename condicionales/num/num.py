5d8423b33a23001bff69182c05b0f0f96e45f9f1
Lucas Emilio <93223042+LucasEmilio@users.noreply.github.com>
2022-01-31 12:03:47 -0600

    Create num.py

---
+++ condicionales/num/num.py
@@ -0,0 +1,13 @@
+#Datos de entrada
+num = int(input("Ingresa un numero entero: "))
+
+#proceso
+if num > 0:
+	#Datos de salida
+	print(f"[{num}] es un numero entero")
+elif num < 0:
+	#Datos de salida
+	print(f"[{num}] es un numero negativo")
+else:
+	#Datos de salido
+	print(f"[{num}] es cero")
