c2960e5cf713256f30146759ffd0d38b89f39159
Lucas Emilio <93223042+LucasEmilio@users.noreply.github.com>
2022-01-31 12:02:50 -0600

    Add files via upload

---
+++ condicionales/numNegPos/numNegPos.c
@@ -0,0 +1,25 @@
+/* Programa que nos dice si un numero es positivo*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int num =0;
+
+	/*Datos de entrada*/
+	printf("Ingresa un numero entero\n");
+	scanf("%d", &num);
+
+
+    /*proceso*/
+	if (num > 0)
+	{	
+		/*Datos de salida*/
+		printf("[%d] es un numero positivo\n", num);
+		
+	} else {
+		/*Datos de salida*/
+		printf("[%d] es un numero negativo\n", num);
+
+	}
+	return 0;
+}
